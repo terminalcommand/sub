7cfb65c852c2834f680632f7205da61bf45c1c2b
terminalcommand <erkin@tuzcular.com>
2016-06-12 16:15:56 +0300

    Added multithreading support for multiple http requests

--- subnew.py
+++ subnew.py
@@ -2,12 +2,17 @@ from moviesearch import MovieSearch
 from subdb import SubDB
 from downloader import Downloader
 
+import concurrent.futures
+
 moviesearcher = MovieSearch()
 subsearcher = SubDB()
 # need to implement addict7ed support
 subdownloader = Downloader()
-for i in moviesearcher.return_files():
-    link = subsearcher.process(i)
-    print(i)
-    print(link)
-    subdownloader.download(link, moviesearcher.get_name(i)+'.srt')
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    for i in moviesearcher.return_files():
+        link = subsearcher.process(i)
+        print(i)
+        print(link)
+        subdownloader.download(link, moviesearcher.get_name(i)+'.srt')
+        executor.submit(subdownloader.download, link, moviesearcher.get_name(i)+'.srt')
